d24d65f4cd9323468186b74be8b4f44a064e4c17
Goupil117 <84926681+Goupil117@users.noreply.github.com>
2021-11-21 05:11:51 +0100

    Add files via upload

--- src/Personnage.h
+++ src/Personnage.h
@@ -1,4 +1,16 @@
 #ifndef DEF_Personnage
 #define DEF_Personnage
 
+typedef struct Joueur
+{
+    char nom[41];
+    unsigned char role;
+    unsigned char comportement;
+    unsigned char vie;
+    unsigned char eau;
+    unsigned char nourriture;
+    unsigned char sommeil;
+    unsigned char evenement_ile;
+}joueur;
+
 #endif
